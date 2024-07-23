ce78e787365b1ec1036cfc33475d04274e10da03
Tomás Matteozzi <tomasmatteozzi@gmail.com>
2024-07-23 12:40:35 -0300

    Footer V2

--- app/pages/Footer.tsx
+++ app/pages/Footer.tsx
@@ -1,20 +1,66 @@
+'use client';
 import { ReactNode } from 'react';
+import { Link as ScrollLink } from 'react-scroll';
 
 interface FooterProps {
     content: {
         text: string;
+        contactDetails: {
+            icon: ReactNode;
+            text: string;
+        }[];
+        links: {
+            name: string;
+            link: string;
+        }[];
     };
 }
 
 export default function Footer({ content }: FooterProps) {
-    const { text } = content;
+    const { text, contactDetails, links } = content;
 
     return (
-        <footer className="bg-gray-50 text-center py-6">
-            <div className="container mx-auto">
-                <p className="text-sm md:text-md lg:text-l font-regular text-gray-400">
-                    {text}
-                </p>
+        <footer className="bg-gray-50 text-gray-700 pt-6">
+            <div className="container mx-auto flex flex-col lg:flex-row justify-start items-start p-6">
+                <div className="p-4">
+                    <h2 className="text-lg font-semibold text-black mb-4">
+                        Enlaces
+                    </h2>
+                    <ul className="space-y-2">
+                        {links.map((link, index) => (
+                            <li key={index} className="flex items-center mb-2">
+                                <ScrollLink
+                                    to={link.link}
+                                    smooth={true}
+                                    duration={500}
+                                    className="text-sm text-gray-600 hover:text-gray-900 cursor-pointer"
+                                >
+                                    {link.name}
+                                </ScrollLink>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+                <div className="p-4">
+                    <h2 className="text-lg font-semibold text-black mb-4">
+                        Datos
+                    </h2>
+                    <div>
+                        {contactDetails.map((item, index) => (
+                            <div key={index} className="flex items-center mb-2">
+                                <div className="mr-3 text-xl text-[#0070f3]">
+                                    {item.icon}
+                                </div>
+                                <p className="text-sm text-gray-600 hover:text-gray-900">
+                                    {item.text}
+                                </p>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+            <div className="w-full p-4 text-center bg-gray-100">
+                <p className="text-sm p-4 font-regular text-gray-500">{text}</p>
             </div>
         </footer>
     );
