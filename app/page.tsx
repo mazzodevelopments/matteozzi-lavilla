6596fb3b424dc684be68a0edf46bb6f722c87fcc
Tomás Matteozzi <tomasmatteozzi@gmail.com>
2024-08-04 12:55:58 -0300

    Staff content

--- app/page.tsx
+++ app/page.tsx
@@ -97,33 +97,47 @@ const nosotros = [
     }
 ];
 
-const staff = [
-    {
-        rol: 'CEO',
-        nombreCompleto: 'Emiliano Matteozzi',
-        foto: emiliano.src
-    },
-    {
-        rol: 'CEO',
-        nombreCompleto: 'Emiliano Matteozzi',
-        foto: emiliano.src
-    },
-    {
-        rol: 'CEO',
-        nombreCompleto: 'Emiliano Matteozzi',
-        foto: emiliano.src
-    },
-    {
-        rol: 'CEO',
-        nombreCompleto: 'Emiliano Matteozzi',
-        foto: emiliano.src
-    },
-    {
-        rol: 'CEO',
-        nombreCompleto: 'Emiliano Matteozzi',
-        foto: emiliano.src
-    }
-];
+const staff = {
+    parrafo:
+        'Nuestro equipo está compuesto por profesionales apasionados y altamente capacitados en sus respectivas áreas. Con una combinación de experiencia, creatividad y compromiso, estamos dedicados a alcanzar la excelencia en todo lo que hacemos.',
+    primarios: [
+        {
+            imagen: 'https://images.unsplash.com/photo-1593642634367-d91a135587b5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=668&q=80',
+            nombre: 'Cesar Matteozzi',
+            rol: 'CEO'
+        },
+        {
+            imagen: 'https://images.unsplash.com/photo-1593642634367-d91a135587b5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=668&q=80',
+            nombre: 'Emiliano Matteozzi',
+            rol: 'CEO'
+        },
+        {
+            imagen: 'https://images.unsplash.com/photo-1593642634367-d91a135587b5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=668&q=80',
+            nombre: 'Stella Lavilla',
+            rol: 'CEO'
+        }
+    ],
+    secundarios: [
+        {
+            id: 1,
+            name: 'John Doe',
+            designation: 'Software Engineer',
+            image: 'https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80'
+        },
+        {
+            id: 2,
+            name: 'Robert Johnson',
+            designation: 'Product Manager',
+            image: 'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60'
+        },
+        {
+            id: 3,
+            name: 'Jane Smith',
+            designation: 'Data Scientist',
+            image: 'https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60'
+        }
+    ]
+};
 
 const footer = {
     texto: 'Powered by © Mazzo Developments - 2024',
@@ -149,7 +163,7 @@ export default function Home() {
             <Info content={info} />
             <Servicios content={servicios} />
             <Nosotros content={nosotros} />
-            {/* <Staff content={staff} /> */}
+            <Staff content={staff} />
             <Contacto />
             <Footer content={footer} />
         </div>
