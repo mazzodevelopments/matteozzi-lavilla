25deee13902d84d948350c6627bfa036041e72bb
Tomás Matteozzi <tomasmatteozzi@gmail.com>
2025-03-04 17:30:29 -0300

    V2

--- app/pages/Team.jsx
+++ app/pages/Team.jsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { useEffect } from "react";
 import SectionContainer from "../components/SectionContainer";
 
@@ -37,49 +39,43 @@ export default function Team() {
         </p>
       </div>
 
-      <div className="team flex flex-col items-center justify-center mt-16 text-center">
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-16 lg:gap-8 w-[90%] lg:w-[80%] text-center mt-16">
-          {/* CARD1 */}
-          <div className="h-[400px] max-w-[400px] w-full mx-auto">
-            <div className="relative flex flex-col h-[280px] rounded-2xl p-6 justify-between bg-gradient-to-r from-[#DEE3CD] to-[#93bec7]">
-              <img
-                src="/member3.png"
-                alt="Member"
-                className="absolute z-20 bottom-0 left-1/2 transform -translate-x-1/2 w-[400px] h-[400px] rounded-xl object-cover"
-              />
-            </div>
-            <h3 className="text-xl font-bold mt-4">César Amílcar Matteozzi</h3>
-            <span className="text-sm sm:text-lg text-gray-600 mt-4 sm:mt-6 uppercase">
-              Co-Fundador
-            </span>
-          </div>
-          {/* CARD2 */}
-          <div className="h-[400px] max-w-[400px] w-full mx-auto">
-            <div className="relative flex flex-col h-[280px] rounded-2xl p-6 justify-between bg-[#93bec7]">
+      <div className="team flex flex-col items-center justify-center mt-16">
+        <div className="w-full max-w-4xl mx-auto flex flex-col lg:flex-row items-center gap-10 lg:gap-16">
+          {/* Featured Member */}
+          <div className="h-[400px] max-w-[400px] w-full mx-auto mt-20">
+            <div className="relative flex flex-col h-[280px] rounded-2xl p-6 justify-between bg-gradient-to-r from-[#93bec7] to-[#46939f]">
               <img
                 src="/member2.png"
-                alt="Member"
+                alt="Featured Member"
                 className="absolute z-20 bottom-0 left-1/2 transform -translate-x-1/2 w-[400px] h-[400px] rounded-xl object-cover"
               />
             </div>
-            <h3 className="text-xl font-bold mt-4">Emiliano Mario Matteozzi</h3>
-            <span className="text-sm sm:text-lg text-gray-600 mt-4 sm:mt-6 uppercase">
+            <h3 className="text-xl font-bold mt-4 text-center">
+              Emiliano Mario Matteozzi
+            </h3>
+            <span className="text-sm sm:text-lg text-gray-600 mt-2 uppercase block text-center">
               Director
             </span>
           </div>
-          {/* CARD3 */}
-          <div className="h-[400px] max-w-[400px] w-full mx-auto">
-            <div className="relative flex flex-col h-[280px] rounded-2xl p-6 justify-between bg-gradient-to-r from-[#93bec7] to-[#46939f]">
-              <img
-                src="/member4.png"
-                alt="Member"
-                className="absolute z-20 bottom-0 left-1/2 transform -translate-x-1/2 w-[400px] h-[400px] rounded-xl object-cover"
-              />
+
+          {/* Quote Section */}
+          <div className="max-w-[500px] w-full">
+            <div className="relative">
+              <svg
+                className="absolute -top-10 -left-2 w-16 h-16 text-[#93bec7] opacity-30"
+                fill="currentColor"
+                viewBox="0 0 32 32"
+              >
+                <path d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z" />
+              </svg>
+              <p className="relative text-lg lg:text-xl italic text-gray-700 mt-6 lg:mt-0 pl-6">
+                Nuestra misión es transformar ideas en soluciones innovadoras
+                que impulsen el crecimiento y éxito de nuestros clientes. Cada
+                proyecto es una oportunidad para demostrar nuestro compromiso
+                con la excelencia y la calidad.
+              </p>
+              <div className="w-16 h-1 bg-[#93bec7] mt-6 ml-6"></div>
             </div>
-            <h3 className="text-xl font-bold mt-4">Stella Maris Lavilla</h3>
-            <span className="text-sm sm:text-lg text-gray-600 mt-4 sm:mt-6 uppercase">
-              Co-Fundador
-            </span>
           </div>
         </div>
       </div>
