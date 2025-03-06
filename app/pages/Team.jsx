923c46cc3fd58cc25ab1f2e92540eee8db08f774
Monza <matiasmonzalvoarg@gmail.com>
2025-03-06 20:02:09 -0300

    quote section

--- app/pages/Team.jsx
+++ app/pages/Team.jsx
@@ -25,42 +25,27 @@ export default function Team() {
   }, []);
 
   return (
-    <SectionContainer id="team">
-      <div className="team flex flex-col items-start justify-start w-full pt-6 lg:pt-0">
-        <span className="text-[2.5vw] sm:text-sm uppercase tracking-wider text-gray-600 mb-4 py-1 px-3 border border-gray-600 rounded-xl">
-          Team
-        </span>
-        <h1 className="text-3xl sm:text-[6.5vw] font-bold leading-tight sm:leading-[1]">
-          Nuestro Equipo
-        </h1>
-        <p className="text-sm lg:text-[0.9vw] text-gray-600 mt-4 sm:mt-6">
-          Conformado por un staff de profesionales altamente capacitados y
-          comprometidos con la innovación y el éxito de cada proyecto.
-        </p>
-      </div>
-
-      <div className="team flex flex-col items-center justify-center mt-16">
-        <div className="w-full max-w-4xl mx-auto flex flex-col lg:flex-row items-center gap-10 lg:gap-16">
+    <section
+      id="team"
+      className="z-50 justify-center items-center py-10"
+      data-scroll-section
+    >
+      <div className="team flex flex-col items-center justify-center lg:px-0 px-6">
+        <div className="relative w-full max-w-4xl mx-auto flex flex-col lg:flex-row items-center  bg-gradient-to-r from-[#93bec7] to-[#46939f] h-screen lg:h-[500px] rounded-3xl overflow-hidden">
           {/* Featured Member */}
-          <div className="h-[400px] max-w-[400px] w-full mx-auto mt-20">
-            <div className="relative flex flex-col h-[280px] rounded-2xl p-6 justify-between bg-gradient-to-r from-[#93bec7] to-[#46939f]">
+          <div className="h-1/2 lg:h-full w-full lg:w-2/5 overflow-hidden">
+            <div className="relative flex flex-col w-full h-full rounded-2xl">
               <img
                 src="/member2.png"
                 alt="Featured Member"
-                className="absolute z-20 bottom-0 left-1/2 transform -translate-x-1/2 w-[400px] h-[400px] rounded-xl object-cover"
+                className="w-[100%] h-[100%] object-cover"
               />
             </div>
-            <h3 className="text-xl font-bold mt-4 text-center">
-              Emiliano Mario Matteozzi
-            </h3>
-            <span className="text-sm sm:text-lg text-gray-600 mt-2 uppercase block text-center">
-              Director
-            </span>
           </div>
 
           {/* Quote Section */}
-          <div className="max-w-[500px] w-full">
-            <div className="relative">
+          <div className="lg:w-3/5 lg:h-full w-full h-1/2 flex justify-center items-center bg-white border-gray-300 border p-10">
+            <div className="relative mt-10">
               <svg
                 className="absolute -top-10 -left-2 w-16 h-16 text-[#93bec7] opacity-30"
                 fill="currentColor"
@@ -75,10 +60,19 @@ export default function Team() {
                 con la excelencia y la calidad.
               </p>
               <div className="w-16 h-1 bg-[#93bec7] mt-6 ml-6"></div>
+
+              <div className=" p-2 z-20 w-full">
+                <span className="text-sm text-black sm:text-lg text-gray-600 mt-2 uppercase block text-left ml-4">
+                  Director
+                </span>
+                <h3 className="text-xl text-black font-bold text-left ml-4">
+                  Emiliano Mario Matteozzi
+                </h3>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </SectionContainer>
+    </section>
   );
 }
